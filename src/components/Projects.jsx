c242336c395db74917279942a6f1b1e1b1a97f6c
Nikhil R <54244227+NikkkhilRam@users.noreply.github.com>
2024-06-12 14:19:33 +0530

    project added

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -7,7 +7,7 @@ function Projects() {
   return (
     <div className="text-white w-full  flex flex-col my-44 p-4">
       <p className="text-[40px] text-center mb-16">Projects</p>
-      <div className="flex flex-wrap  justify-between gap-3">
+      <div className="flex flex-wrap  justify-around gap-3">
         {PROJECTS.map((project, index) => {
           return (
             <ProjectCard
--- src/utils/projectList.js
+++ src/utils/projectList.js
@@ -1,5 +1,11 @@
 import coinPulseX from "../assets/coinpulseX.png";
 import wordleClone from "../assets/wordle-clone.png";
+import wca from "../assets/wca.png"
+
+import geminiGourmet from "../assets/geminiGourmet.png";
+import bitbrew from "../assets/bitbrew1.png"
+
+
 
 export const PROJECTS = [
   {
@@ -9,10 +15,28 @@ export const PROJECTS = [
     img: coinPulseX,
   },
   {
+    title: "Gemini Gourmet",
+    desc: "AI Web application that generates Recipe based on users preference. Also provides youtube videos for the recipe generated using the Youtube Data API v3",
+    link: "https://github.com/NikkkhilRam/gemini-gourmet-fe",
+    img: geminiGourmet,
+  },
+  {
+    title: "Bit Brew",
+    desc: "A simple full stack blogging application which lets user write articles, stories, experiance etc. The Full stack application is built with MERN stack along with Tailwind CSS. ",
+    link: "https://github.com/NikkkhilRam/frontend-bitbrew",
+    img: bitbrew,
+  },
+  {
     title: "Wordle Clone",
     desc: "Developed a responsive Wordle clone using React.js and Tailwind CSS, featuring customizable difficulty levels to enhance user engagement. Demonstrated expertise in modern front-end technologies and design principles.",
     link: "https://wordle-clone-jade.vercel.app/",
     img: wordleClone,
+  },
+  {
+    title: "Whatsapp Chat Analyzer - Python Project",
+    desc: "A simple web app that lets you upload a whatsapp chat .txt file and gives you various insights of the chat. Some of the insights are Total messages, Most used emoji, Most interacted user, Timeline of interaction etc. ",
+    link: "https://nikkkhilram-wca-main-ysjl0c.streamlit.app/",
+    img: wca,
   },
  
 ];
